114855762ebecbc5f697d9e9fd0ebe7de7c1dadb
Marcio Fernandes <farciomernandes@gmail.com>
2023-03-15 10:11:24 -0300

    study: loops

---
+++ 21/main.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+	/*
+
+		//Array normal
+		for i := 0; i < 10; i++ {
+			println(i)
+		}
+
+		numeros := []string{"um", "dois", "três"}
+
+		//Pode trocar o KEY ou VALUE por "_" caso nao queira usar esse valor
+		for key, value := range numeros {
+			println(key, value)
+		}
+
+		//Um especie de WHILE no Go
+		number := 10
+		for 1 < 10 {
+			print(number)
+			number++
+		} */
+
+	// Loop infinito
+	for {
+		println("Hello World!")
+	}
+}
