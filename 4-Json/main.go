8cb23e5d49ceecabff0d8c1bc2e194cf2398eeda
Marcio Fernandes <farciomernandes@gmail.com>
2023-03-20 15:55:06 -0300

    refact: rename files

---
+++ 4-Json/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Conta struct {
+	Numero int `json: numero` //Singifica que um "numero" no JSON se refere ao Saldo
+	Saldo  int `json: saldo`  //Singifica que um "saldo" no JSON se refere ao Saldo
+	//Pode colocar `json: -` parar ignorar algo especifico
+}
+
+func main() {
+
+	// CONVERTENDO STRUCT EM JSON
+
+	conta := Conta{Numero: 1, Saldo: 100}
+	res, err := json.Marshal(conta) //Transforma STRUCT em JSON, mas retorna em BYTE
+
+	if err != nil {
+		println(err)
+	}
+	println(string(res))
+
+	err = json.NewEncoder(os.Stdout).Encode(conta)
+
+	if err != nil {
+		println(err)
+	}
+	// ou -> encoder := json.NewEncoder(os.Stdout) + encoder.Encode(conta)
+
+	// -----------------------------------------------------------------------
+
+	// CONVERTENDO JSON EM STRUCT
+
+	jsonPuro := []byte(`"Numero":2, "Saldo": 150`)
+	var contaX Conta
+	//Converter JSON em STRUCT
+	err = json.Unmarshal(jsonPuro, &contaX) // Envia o endereço na memória no lugar do nome da variável
+
+	if err != nil {
+		println(err)
+	}
+
+	println(contaX.Saldo)
+}
