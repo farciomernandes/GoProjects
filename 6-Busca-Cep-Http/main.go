8694af431c7408e8e457f4e81bc3ff8360ab88d8
Marcio Fernandes <farciomernandes@gmail.com>
2023-03-21 10:25:22 -0300

    study: create validation and SET HEADER and response

--- 6-Busca-Cep-Http/main.go
+++ 6-Busca-Cep-Http/main.go
@@ -24,5 +24,16 @@ func main() {
 }
 
 func BuscaCEP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	cepParam := r.URL.Query().Get("cep")
+	if cepParam == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello World!" + cepParam))
 }
